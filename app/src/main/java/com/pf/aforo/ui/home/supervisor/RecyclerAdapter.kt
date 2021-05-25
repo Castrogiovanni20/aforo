96a6c5a1203bdd9685deda6a49d71f5eef2a7077
Castrogiovanni20 <ramirocastrogiovanni@gmail.com>
2021-05-25 02:04:21 -0300

    Random profile background on CardView

--- app/src/main/java/com/pf/aforo/ui/home/supervisor/RecyclerAdapter.kt
+++ app/src/main/java/com/pf/aforo/ui/home/supervisor/RecyclerAdapter.kt
@@ -1,10 +1,10 @@
 package com.pf.aforo.ui.home.supervisor
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.navigation.NavController
@@ -12,10 +12,12 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.pf.aforo.R
 import com.pf.aforo.data.model.UserFuncionario
+import de.hdodenhof.circleimageview.CircleImageView
 
 internal class RecyclerAdapter(private var listFuncionarios: ArrayList<UserFuncionario>) :
     RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>() {
     internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var imageProfile: CircleImageView = view.findViewById(R.id.circleImageViewProfile)
         var fullName: TextView = view.findViewById(R.id.textViewFullName)
         var role: TextView = view.findViewById(R.id.textViewRole)
         var imgButtonEdit: ImageButton = view.findViewById(R.id.imageButtonEditar)
@@ -23,7 +25,15 @@ internal class RecyclerAdapter(private var listFuncionarios: ArrayList<UserFunci
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         var navController: NavController? = null
+
+        var array = intArrayOf(
+            R.drawable.woman_perfil, R.drawable.woman_profile_2, R.drawable.woman_profile_3,
+            R.drawable.man_profile, R.drawable.man_profile_2, R.drawable.man_profile_3, R.drawable.man_profile_4)
+        val randomNumber = (0 until array.size).random()
+
         val item = listFuncionarios[position]
+
+        holder.imageProfile.setImageResource(array[randomNumber])
         holder.fullName.text = item.firstName + " " + item.lastName
         holder.role.text = item.role.replace("_", " ")
 
