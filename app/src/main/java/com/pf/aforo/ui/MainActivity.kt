42e86a1a81fc49c633ed61c5e65045dbc00ba56a
Monica G. Bidart <bidartmgb@hotmail.com>
2021-06-19 16:28:19 -0300

    mensaje emergente de alerta

--- app/src/main/java/com/pf/aforo/ui/MainActivity.kt
+++ app/src/main/java/com/pf/aforo/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.pf.aforo.ui
 
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.pf.aforo.R
 
@@ -10,6 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val builder = AlertDialog.Builder(this)
+        builder.setIcon(R.drawable.lights)
+        builder.setTitle("ALERTA EN C5N!!!")
+        builder.setMessage("LA CAPACIDAD DE AFORO ESTÁ CASI EN EL LÍMITE, POR FAVOR TOMAR MEDIDAS CONTINGENTES...")
+        builder.setPositiveButton("INFORMADO", { dialogInterface: DialogInterface, i ->
+            // Ver la leyenda de INFORMADO o poner ACEPTAR, usar el semáforo como icono
+            // Acá podemos redireccionar a otro código
+        })
+        // builder.setNegativeButton("CANCELAR", { dialogInterface:DialogInterface, i -> }) Posibilidad de otro botón para ejecución alternativa
+        builder.show()
     }
 
 }
